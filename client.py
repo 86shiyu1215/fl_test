9f581fa6be67a574d1c3ca066b0da9f093d3aba2
86shiyu1215 <86shiyu1215@gmail.com>
2026-06-24 15:04:40 +0900

    Update client.py

--- client.py
+++ client.py
@@ -1,1 +1,18 @@
-client.py
+import torch  #pytorch を使うための準備
+import torch.nn as nn  #ニューラルネットワーク用
+
+# シンプルなモデル（1入力→1出力）
+model = nn.Linear(1, 1)  #入力１つ，出力１つ，のモデルで行くで
+
+print("model created") #動作確認
+import numpy as np
+
+# データ作成（100個のデータ）
+x = np.random.rand(100, 1)  # 入力データ（0〜1のランダム）
+y = 3 * x + 2               # 出力データ（関係式）
+
+# torch用に変換
+x_train = torch.tensor(x, dtype=torch.float32)
+y_train = torch.tensor(y, dtype=torch.float32)
+
+print("data created")
