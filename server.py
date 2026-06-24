1b36c826068795e6adb9ebb3044d0a9cb3fbddae
86shiyu1215 <86shiyu1215@gmail.com>
2026-06-24 19:07:59 +0900

    Update server.py

--- server.py
+++ server.py
@@ -3,6 +3,6 @@ import flwr as fl
 # サーバ起動
 fl.server.start_server(
     server_address="0.0.0.0:8080",
-    config=fl.server.ServerConfig(num_rounds=3),
+    config=fl.server.ServerConfig(num_rounds=10),
 )
 
